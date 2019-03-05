78337b929789d457a15785762d72cc7256dcc1e0
minjae <32402820+kkrlee@users.noreply.github.com>
2019-03-05 17:19:01 -0500

    first upload

---
+++ codeitcodeit_tower_of_hanoi.py
@@ -0,0 +1,14 @@
+def move_disk(disk_num, start_peg, end_peg):
+    print("%d번 원판을 %d번 기둥에서 %d번 기둥으로 이동" % (disk_num, start_peg, end_peg))
+
+def hanoi(num_discs, start_peg = 1, end_peg= 3):
+    if num_discs == 0:
+        return
+    else:
+        other_peg = 6 - start_peg - end_peg
+        hanoi(num_discs - 1, start_peg, other_peg)
+        move_disk(num_discs, start_peg, end_peg)
+        hanoi(num_discs - 1, other_peg, end_peg)
+    return 
+
+hanoi(3, 1, 3)
